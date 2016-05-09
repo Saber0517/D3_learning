026779756f995b18910acc11b342b61bc9de3138
zhangja4 <james.zhang@oocl.com>
2016-05-09 17:40:40 +0800

    incomplete line sample

--- module/js/index.js
+++ module/js/index.js
@@ -3,6 +3,17 @@ var svg = d3.select("#container").append("svg")
     //width,height
     .attr("width", 500).attr("height", 250);
 
-d3.select("svg")
+var g = d3.select("svg")
     .append("g")
     .attr("transform","translate(50,30)");
+
+var data = [1, 3, 5, 7, 8, 4, 3, 7];
+
+var line_generator = d3.svg.line()
+.x(function (d, i) {return i;})
+.y(function (d) {return d;})
+
+
+d3.select("g")
+.append("path")
+.attr("d",line_generator(data)) 
